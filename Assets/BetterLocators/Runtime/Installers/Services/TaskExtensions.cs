6332c6ba3295ad06d4934f08fc82a0c49665365c
uurhaa <uurha.dev@gmail.com>
2024-01-24 02:08:54 +0100

    Add TODO

--- Assets/BetterLocators/Runtime/Installers/Services/TaskExtensions.cs
+++ Assets/BetterLocators/Runtime/Installers/Services/TaskExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
+//TODO: Move to Better Extensions
 public static class TaskExtensions
 {
     public static Task WhenAll(this IEnumerable<Task> tasks)
