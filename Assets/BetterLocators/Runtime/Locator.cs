7ffbdd02cb8a856e8ddef00edb6c629a5ab8fa8b
Anatoly Brizhan <anatoly.brizhan@gmail.com>
2024-03-05 18:01:05 +0100

    Add public validation method

--- Assets/BetterLocators/Runtime/Locator.cs
+++ Assets/BetterLocators/Runtime/Locator.cs
@@ -11,8 +11,7 @@ namespace Better.Locators.Runtime
         public virtual void Register<T>(T item) where T : TItem
         {
             var type = item.GetType();
-
-            if (_services.ContainsKey(type))
+            if (HasRegistered<T>())
             {
                 var message = $"Service of type {type} is already registered. Operation cancelled";
                 Debug.LogError(message);
@@ -22,11 +21,16 @@ namespace Better.Locators.Runtime
             _services[type] = item;
         }
 
+        public virtual bool HasRegistered<T>() where T : TItem
+        {
+            var type = typeof(T);
+            return _services.ContainsKey(type);
+        }
+
         public virtual void Unregister<T>(T service) where T : TItem
         {
             var type = service.GetType();
-
-            if (!_services.ContainsKey(type))
+            if (!HasRegistered<T>())
             {
                 var message = $"Item of type {type} is not registered. Operation cancelled";
                 Debug.LogError(message);
