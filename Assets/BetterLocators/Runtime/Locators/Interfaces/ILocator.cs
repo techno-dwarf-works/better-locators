723e2013d9a8a634889ab52e436cdc72c84af306
Anatoly Brizhan <anatoly.brizhan@gmail.com>
2024-03-07 00:02:13 +0100

    Update dependency and handlers

---
+++ Assets/BetterLocators/Runtime/Locators/Interfaces/ILocator.cs
@@ -0,0 +1,10 @@
+﻿namespace Better.Locators.Runtime
+{
+    public interface ILocator<in TItem>
+    {
+        void Register<T>(T item) where T : TItem;
+        bool HasRegistered<T>() where T : TItem;
+        void Unregister<T>(T item) where T : TItem;
+        T Get<T>() where T : TItem;
+    }
+}
