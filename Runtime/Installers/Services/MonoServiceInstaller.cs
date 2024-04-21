a91fc3e2e828073b37f018d5582080d37a54f1d2
OpOpYaDev <OpOpYaDev@users.noreply.github.com>
2024-04-21 17:10:35 +0000

    Merge pull request #11 from techno-dwarf-works/feature/refactoring
    Version 0.0.8

--- Runtime/Installers/Services/MonoServiceInstaller.cs
+++ Runtime/Installers/Services/MonoServiceInstaller.cs
@@ -1,12 +1,16 @@
 #if BETTER_SERVICES
 using System;
 using Better.Services.Runtime;
+using UnityEngine;
 
 namespace Better.Locators.Runtime.Installers
 {
     [Serializable]
     public class MonoServicesInstaller : ServicesInstaller<MonoService>
     {
+        [SerializeField] private MonoService[] _services;
+        
+        protected override MonoService[] Services => _services;
     }
 }
 #endif
