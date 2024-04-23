923ce3a82740c2eefa04bca6f6a56d3e6febe90c
uurha <uurha@users.noreply.github.com>
2024-04-23 22:23:48 +0000

    Merge pull request #14 from techno-dwarf-works/feature/refactoring
    Fix Locator Item unregistration

--- Runtime/Locators/Interfaces/ILocator.cs
+++ Runtime/Locators/Interfaces/ILocator.cs
@@ -1,10 +1,15 @@
-﻿namespace Better.Locators.Runtime
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Better.Services.Runtime.Interfaces;
+
+namespace Better.Locators.Runtime
 {
-    public interface ILocator<in TItem>
+    public interface ILocator<TItem>
     {
         void Register<T>(T item) where T : TItem;
         bool HasRegistered<T>() where T : TItem;
         void Unregister<T>(T item) where T : TItem;
         T Get<T>() where T : TItem;
+        Task<T> GetAsync<T>(CancellationToken token) where T : TItem;
     }
 }
---
+++ Samples~/TestSamples/Scripts/TestGetService.cs
@@ -0,0 +1,16 @@
+﻿using Better.Attributes.Runtime;
+using Better.Locators.Runtime;
+using UnityEngine;
+
+namespace Samples.TestSamples
+{
+    public class TestGetService : MonoBehaviour
+    {
+        [EditorButton]
+        private async void Test()
+        {
+            var t = await ServiceLocator.GetAsync<TestService>();
+            Debug.Log(t.GetType().Name);
+        }
+    }
+}
