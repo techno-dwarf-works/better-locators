f79e267c83956e74d371b2ee2b7e5d551d38d914
OpOpYaDev <OpOpYaDev@users.noreply.github.com>
2024-05-17 21:34:38 +0000

    Merge pull request #21 from techno-dwarf-works/feature/refactoring
    Version 0.1.8

---
+++ Runtime/Extensions/ILocatorExtensions.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Better.Commons.Runtime.Utility;
+using Better.Locators.Runtime;
+
+public static class ILocatorExtensions
+{
+    public static bool TryAdd<TElement>(this ILocator<Type, TElement> self, TElement element)
+    {
+        if (self == null)
+        {
+            DebugUtility.LogException<ArgumentNullException>(nameof(self));
+            return false;
+        }
+
+        if (element == null)
+        {
+            DebugUtility.LogException<ArgumentNullException>(nameof(element));
+            return false;
+        }
+
+        var key = element.GetType();
+        return self.TryAdd(key, element);
+    }
+
+    public static void Add<TKey, TElement>(this ILocator<TKey, TElement> self, TKey key, TElement element)
+    {
+        if (self == null)
+        {
+            DebugUtility.LogException<ArgumentNullException>(nameof(self));
+            return;
+        }
+
+        if (key == null)
+        {
+            DebugUtility.LogException<ArgumentNullException>(nameof(key));
+            return;
+        }
+
+        if (element == null)
+        {
+            DebugUtility.LogException<ArgumentNullException>(nameof(element));
+            return;
+        }
+
+        if (!self.TryAdd(key, element))
+        {
+            var message = $"Invalid adding {nameof(element)} by {nameof(key)}({key})";
+            DebugUtility.LogException<ArgumentNullException>(message);
+        }
+    }
+
+    public static void Add<TElement>(this ILocator<Type, TElement> self, TElement element)
+    {
+        if (element == null)
+        {
+            DebugUtility.LogException<ArgumentNullException>(nameof(element));
+            return;
+        }
+
+        var key = element.GetType();
+        self.Add(key, element);
+    }
+
+    public static bool ContainsKey<TElement, TDerived>(this ILocator<Type, TElement> self)
+        where TDerived : TElement
+    {
+        var key = typeof(TDerived);
+        return self.ContainsKey(key);
+    }
+
+    public static bool TryGet<TKey, TElement, TDerived>(this ILocator<TKey, TElement> self, TKey key, out TDerived element)
+        where TDerived : TElement
+    {
+        if (self == null)
+        {
+            DebugUtility.LogException<ArgumentNullException>(nameof(self));
+            element = default;
+            return false;
+        }
+
+        if (self.TryGet(key, out var located)
+            && located is TDerived casted)
+        {
+            element = casted;
+            return true;
+        }
+
+        element = default;
+        return false;
+    }
+
+    public static bool TryGet<TElement, TDerived>(this ILocator<Type, TElement> self, out TDerived element)
+        where TDerived : TElement
+    {
+        var key = typeof(TDerived);
+        return self.TryGet(key, out element);
+    }
+
+    public static TElement Get<TKey, TElement>(this ILocator<TKey, TElement> self, TKey key)
+    {
+        if (self == null)
+        {
+            DebugUtility.LogException<ArgumentNullException>(nameof(self));
+            return default;
+        }
+
+        if (!self.TryGet(key, out var element))
+        {
+            var message = $"Invalid getting {nameof(element)} by {nameof(key)}({key})";
+            DebugUtility.LogException<InvalidOperationException>(message);
+            return default;
+        }
+
+        return element;
+    }
+
+    public static TDerived Get<TElement, TDerived>(this ILocator<Type, TElement> self)
+        where TDerived : TElement
+    {
+        if (self == null)
+        {
+            DebugUtility.LogException<ArgumentNullException>(nameof(self));
+            return default;
+        }
+
+        var type = typeof(TDerived);
+        var element = (TDerived)self.Get(type);
+        if (element == null)
+        {
+            var message = $"Invalid getting {nameof(element)} by {nameof(type)}({type})";
+            DebugUtility.LogException<InvalidOperationException>(message);
+        }
+
+        return element;
+    }
+}
---
+++ Runtime/Implementations/MonoLocator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using UnityEngine;
+
+namespace Better.Locators.Runtime
+{
+    public abstract class MonoLocator<TKey, TElement> : MonoBehaviour, ILocator<TKey, TElement>
+    {
+        public event Action Changed
+        {
+            add => Source.Changed += value;
+            remove => Source.Changed -= value;
+        }
+
+        public event Locator<TKey, TElement>.OnElementChanged ElementAdded
+        {
+            add => Source.ElementAdded += value;
+            remove => Source.ElementAdded -= value;
+        }
+
+        public event Locator<TKey, TElement>.OnElementChanged ElementRemoved
+        {
+            add => Source.ElementRemoved += value;
+            remove => Source.ElementRemoved -= value;
+        }
+
+        private ILocator<TKey, TElement> _source;
+
+        public int Count => Source.Count;
+
+        protected ILocator<TKey, TElement> Source
+        {
+            get
+            {
+                if (_source == null)
+                {
+                    _source = new Locator<TKey, TElement>();
+                }
+
+                return _source;
+            }
+        }
+
+        public bool TryAdd(TKey key, TElement element)
+        {
+            return Source.TryAdd(key, element);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return Source.ContainsKey(key);
+        }
+
+        public bool ContainsElement(TElement element)
+        {
+            return Source.ContainsElement(element);
+        }
+
+        public TKey[] GetKeys()
+        {
+            return Source.GetKeys();
+        }
+
+        public TElement[] GetElements()
+        {
+            return Source.GetElements();
+        }
+
+        public bool TryGet(TKey key, out TElement element)
+        {
+            return Source.TryGet(key, out element);
+        }
+
+        public bool Remove(TKey key)
+        {
+            return Source.Remove(key);
+        }
+
+        public bool Remove(TElement element)
+        {
+            return Source.Remove(element);
+        }
+
+        public void Clear()
+        {
+            Source.Clear();
+        }
+    }
+
+    public abstract class MonoLocator<TElement> : MonoLocator<Type, TElement>
+    {
+    }
+
+    public class MonoLocator : MonoLocator<Type, MonoBehaviour>
+    {
+    }
+}
